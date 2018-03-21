1bc17a8478f24efb26160f170255db163d36d20f
cherrybeans <sigridkvamme@gmail.com>
2018-03-21 09:07:31 +0100

    Change AbakusLogo component to use styled-components

--- src/components/AbakusLogo/index.js
+++ src/components/AbakusLogo/index.js
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Img from "react-image";
 import logo from "public/logo-dark.png";
-import "./AbakusLogo.css";
+import styled from "styled-components";
 
-class AbakusLogo extends Component {
-  render() {
-    return <Img className="size" src={logo} />;
-  }
-}
+const Logo = ({ className }) => <Img className={className} src={logo} />;
+
+const AbakusLogo = styled(Logo).attrs({
+  size: props => props.size || "8em"
+})`
+  padding: 1.5em;
+  object-fit: scale-down;
+  max-height: ${props => props.size};
+`;
 
 export default AbakusLogo;
